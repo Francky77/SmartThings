5cc4fb7385cd81eef0a1fd77833749dc4425a825
Francky77 <franck.pejoux@gmail.com>
2016-03-16 23:45:30 -0400

    Update onkyoIP.groovy

--- onkyoIP.groovy
+++ onkyoIP.groovy
@@ -41,6 +41,7 @@ metadata {
 	command "pc"
 	command "net"
 	command "aux"
+    command "fm"
 	command "z2on"
 	command "z2off"
 	command "makeNetworkId", ["string","string"]
@@ -51,19 +52,19 @@ simulator {
 	}
 
 tiles {
-	standardTile("switch", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+		standardTile("switch", "device.switch", width: 1, height: 1, canChangeIcon: true) {
         	state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
         	state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-   		}
+   			}
         standardTile("mute", "device.switch", inactiveLabel: false, decoration: "flat") {
-		state "unmuted", label:"mute", action:"mute", icon:"st.custom.sonos.unmuted", backgroundColor:"#ffffff", nextState:"muted"
-		state "muted", label:"unmute", action:"unmute", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff", nextState:"unmuted"
+			state "unmuted", label:"mute", action:"mute", icon:"st.custom.sonos.unmuted", backgroundColor:"#ffffff", nextState:"muted"
+			state "muted", label:"unmute", action:"unmute", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff", nextState:"unmuted"
         	}
         standardTile("cable", "device.switch", decoration: "flat"){
         	state "cable", label: 'cable', action: "cable", icon:"st.Electronics.electronics3"
         	}
         standardTile("stb", "device.switch", decoration: "flat"){
-        	state "stb", label: 'shield', action: "stb", icon:"st.Electronics.electronics5"
+        	state "stb", label: 'xbox', action: "stb", icon:"st.Electronics.electronics5"
         	}
         standardTile("pc", "device.switch", decoration: "flat"){
         	state "pc", label: 'pc', action: "pc", icon:"st.Electronics.electronics18"
@@ -77,23 +78,23 @@ tiles {
        	standardTile("fm", "device.switch", decoration: "flat"){
         	state "fm", label: 'fm', action: "fm", icon:"st.Electronics.electronics6"
         	}
-	controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range:"(0..70)") {
-		state "level", label:'${currentValue}', action:"setLevel", backgroundColor:"#ffffff"
-		}
+		controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range:"(0..70)") {
+			state "level", label:'${currentValue}', action:"setLevel", backgroundColor:"#ffffff"
+			}
         standardTile("zone2", "device.switch", inactiveLabel: false, decoration: "flat") {
-		state "off", label:"Enable Zone 2", action:"z2on", icon:"st.custom.sonos.unmuted", backgroundColor:"#ffffff", nextState:"on"
-		state "on", label:"Disable Zone 2", action:"z2off", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff", nextState:"off"
+			state "off", label:"Enable Zone 2", action:"z2on", icon:"st.custom.sonos.unmuted", backgroundColor:"#ffffff", nextState:"on"
+			state "on", label:"Disable Zone 2", action:"z2off", icon:"st.custom.sonos.muted", backgroundColor:"#ffffff", nextState:"off"
         	}
         /*   Commenting this out as it doesn't work yet     
         valueTile("currentSong", "device.trackDescription", inactiveLabel: true, height:1, width:3, decoration: "flat") {
-		state "default", label:'${currentValue}', backgroundColor:"#ffffff"
-		}
+			state "default", label:'${currentValue}', backgroundColor:"#ffffff"
+			}
 	*/
 	}
 
 	
     main "switch"
-    details(["switch","mute","cable","stb","pc","net","aux","levelSliderControl","zone2"])
+    details(["switch","mute","cable","stb","pc","net","aux","fm","levelSliderControl","zone2"])
     //Add currentSong to above once I can figure out how to get the QSTN commands parsed into artist/song titles
 }
 
